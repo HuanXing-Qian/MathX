886b55f1ff9ae46b0454ca37d6072db1f291af18
石雨泽 <3832655836@qq.com>
2025-08-14 12:52:35 +0800

    Add files via upload

--- Calc.h
+++ Calc.h
@@ -350,163 +350,6 @@ inline cld complex_gamma(cld z) {
 	return sqrt_2_pi * pow(t, z + 0.5L) * exp(-t) * x;
 }
 
-inline cld diff_calc(string str,const cld z,const string&var_name)
-{
-	complex_variables[var_name]=z;
-	str=ns(str);
-	stack<string> st;
-	queue<string> q;
-	ll i=0;
-	while (i<str.length())
-	{
-		string token;
-		while (i<str.length() && str[i] != ' ') token += str[i++];
-		if (token.empty())
-		{
-			i++;
-			continue;
-		}
-		if (complex_variables.find(token) != complex_variables.end())q.push(token);
-		else if (isdigit(token[0])|| (token[0] == '-' && isdigit(token[1]))||token=="i")q.push(token);
-		else if (prio(token) == 4||prio(token)==6) st.push(token);
-		else if (token == "^") {
-			while (!st.empty() && st.top() != "(" && prio(token) < prio(st.top())) {
-				q.push(st.top());
-				st.pop();
-			}
-			st.push(token);
-		} else if (token == "+" || token == "-" || token == "*" || token == "/") {
-			while (!st.empty() && st.top() != "(" && prio(token) <= prio(st.top())) {
-				q.push(st.top());
-				st.pop();
-			}
-			st.push(token);
-		} else if (prio(token) == 5) q.push(constants.at(token));
-		else if (token[0] == '-' && prio(token.substr(1)) == 5) {
-			string const_name = token.substr(1);
-			q.push("-" + constants.at(const_name));
-		} else if (token == "(") st.push(token);
-		else if (token == ")") {
-			while (!st.empty() && st.top() != "(") {
-				q.push(st.top());
-				st.pop();
-			}
-			if (!st.empty() && st.top() == "(") st.pop();
-			if (!st.empty() && prio(st.top()) == 4) {
-				q.push(st.top());
-				st.pop();
-			}
-		}
-		else {
-			if (lagg == 1)out("Expression error.\n", RED);
-			else if (lagg == 2)out("伙计，表达式都写错了。\n", RED);
-			else if (lagg == 3)out("Ошибка выражения.\n", RED);
-			return 0;
-		}
-		i++;
-	}
-	while (!st.empty())
-	{
-		q.push(st.top());
-		st.pop();
-	}
-	if (rpn)
-	{
-		cout<<"RPN: ";
-		auto nq = q;
-		while (!nq.empty())
-		{
-			cout<<nq.front()<<" ";
-			nq.pop();
-		}
-		cout<<"\n";
-	}
-	stack<pair<cld,cld>> stk;
-	while (!q.empty())
-	{
-		string token = q.front();
-		q.pop();
-		if (complex_variables.find(token) != complex_variables.end())stk.push({complex_variables[token],token!=var_name?0:1});
-		else if (isdigit(token[0])|| (token[0] == '-' && isdigit(token[1])) || token=="i")
-		{
-			cld n;
-			if (token=="i")n.imag(1);
-			else n.real(stold(token));
-			stk.push({n,0});
-		}else if (prio(token)<4)
-		{
-			if (stk.size() < 2) {
-				if (lagg == 1)out("Error: Insufficient " + token + " operators.\n", RED);
-				else if (lagg == 2)out("没有足够的" + token + "操作数，即有左没右或者反之。\n", RED);
-				else if (lagg == 3)out("Ошибка: не хватает операторов " + token + " для выполнения операции.\n", RED);
-				return 0;
-			}
-			auto r=stk.top();
-			stk.pop();
-			auto l=stk.top();
-			stk.pop();
-			pair<cld,cld> result;
-			if (token == "+") result = {l.first+r.first,l.second+r.second};
-			else if (token == "-") result = {l.first-r.first,l.second-r.second};
-			else if (token == "*") result = {l.first*r.first,l.second*r.first+l.first*r.second};
-			else if (token == "/") {
-				if (r.first==cld(0))
-				{
-					out("inf\n",YELLOW);
-					return 0;
-				}
-				result = {l.first/r.first, (r.first*l.second-l.first*r.second)/r.first/r.first};
-			} else if (token == "^") result = {pow(l.first,r.first),pow(l.first,r.first)*(r.second*log(l.first)+r.first*l.second/l.first)};
-			stk.push(result);
-		}else if (prio(token)==4)
-		{
-			if (stk.empty()) {
-				out("No " + token + " function.\n", RED);
-				return 0;
-			}
-			pair<cld,cld> res;
-			ll index = distance(begin(func), find(begin(func), end(func), token));
-			if (yuan[index]==1)
-			{
-				auto x = stk.top();
-				stk.pop();
-				cld z=x.first;
-				cld dz = x.second;
-				if (token=="sin")res = {sin(z),cos(z)*dz};
-				else if (token=="cos")res = {cos(z),-sin(z)*dz};
-				else if (token=="tan")res = {tan(z),(cld(1)+tan(z)*tan(z))*dz};
-				else if (token=="arcsin")res = {asin(z),dz/sqrt(cld(1)-z*z)};
-				else if (token=="arccos")res = {acos(z), -dz / sqrt(cld(1) - z * z)};
-				else if (token=="arctan")res = {atan(z),dz / (cld(1) + z * z)};
-				else if (token=="sinh")res = {sinh(z),cosh(z)*dz};
-				else if (token=="cosh")res = {cosh(z),sinh(z)*dz};
-				else if (token=="tanh")res = {tanh(z),(cld(1) - tanh(z) * tanh(z)) * dz};
-				else if (token=="sqrt")res = {sqrt(z),dz/(cld(2)*sqrt(z))};
-				else if (token=="exp")res = {exp(z),exp(z)*dz};
-				else if (token=="log"||token=="ln")res = {log(z),dz/z};
-				else if (token=="log10")res = {log10(z),dz/z/cld(log(10))};
-				else if (token == "abs") {
-					cld abs_u = abs(z);
-					if (abs_u.imag()==0&&abs_u.real()==0) {
-						out("Error: Derivative of abs(0) is undefined\n", RED);
-						return 0;
-					}
-					cld deriv = (z * conj(dz)).real() / abs_u;
-					res = { abs_u, deriv };
-				}
-				else if (token=="sinc")res = {z.imag()==0&&z.real()==0 ? cld(1) : sin(z)/z,dz*(z*cos(z)-sin(z)/z/z)};
-				else if (token=="Gamma")
-				{
-					const ld h = 5e-7;
-					res = {complex_gamma(z),dz*(complex_gamma(z+h)-complex_gamma(z-h))/cld(2)};
-				}
-			}
-			stk.push(res);
-		}
-	}
-	return stk.top().second;
-}
-
 inline cld complex_calc(string str)
 {
 	str=ns(str);
@@ -759,8 +602,12 @@ inline string diff_calc_fu(string str,string var_name)
 			{
 				if (static_cast<ll>(n.real() != n.real()))stk.push({to_string(n.real()),"0"});
 				else stk.push({to_string(ll(n.real())),"0"});
+			}else if (n.real()==0)
+			{
+				if (static_cast<ll>(n.imag() != n.imag()))stk.push({to_string(n.imag())+"i","0"});
+				else stk.push({to_string(ll(n.imag()))+"i","0"});
 			}
-			else stk.push({to_string(n.real())+"+"+to_string(n.imag())+"i","0"});
+			else stk.push({"("+to_string(n.real())+(n.imag()<0?"-":"+")+to_string(abs(n.imag()))+"i)","0"});
 		}
 		else if (prio(token)<4)
 		{
@@ -860,105 +707,106 @@ inline string diff_calc_fu(string str,string var_name)
 			}
 			else if (token == "^")
 			{
-				string first_;
-				string second_;
-				for (auto v:l.first)if (!isdigit(v)&&v!='.')
+				string first;
+				string second;
+				bool f2=false;
+
+				for (auto v:l.first)if (!isdigit(v)&&v!='.'&&v!='i'&&v!='+'&&v!='('&&v!=')'&&v!='*')
 				{
-					first_ = "("+l.first+")";
-					break;
+					if (l.first.size()!=1)first="("+l.first+")";
+					else first=l.first;
 				}
-				if (first_.empty())first_ = l.first;
-				bool f=1;
-				for (auto v:r.first)if (!isdigit(v)&&v!='.')
+				if (first.empty())first=l.first;
+				for (auto v:r.first)if (!isdigit(v)&&v!='.'&&v!='i'&&v!='+'&&v!='('&&v!=')'&&v!='*')
 				{
-					first_ += "^("+r.first+")";
-					f=0;
-					break;
+					if (r.first.size()!=1)first+="^("+r.first+")";
+					else first+="^"+r.first;
+					f2=1;
 				}
-				if (f)
-				{
-					if (r.first=="1")first_=l.first;
-					else if (r.first=="0")first_="1";
-					else first_ += r.first;
-				}
+				if (!f2)first+="^"+r.first;
+				
 				bool type1=0,type2=0;
 				
-				for (auto v:l.first)if (!isdigit(v)&&v!='.'&&v!='i'&&v!='+'&&v!='('&&v!=')')
+				for (auto v:l.first)if (!isdigit(v)&&v!='.'&&v!='i'&&v!='+'&&v!='('&&v!=')'&&v!='*')
 				{
 					type1=1;
 					break;
 				}
-				for (auto v:r.first)if (!isdigit(v)&&v!='.'&&v!='i'&&v!='+'&&v!='('&&v!=')')
+				for (auto v:r.first)if (!isdigit(v)&&v!='.'&&v!='i'&&v!='+'&&v!='('&&v!=')'&&v!='*')
 				{
 					type2=1;
 					break;
 				}
 				// cout << type1 << " " << type2 << "\n";
-				if (type1&&!type2) // x^a r.first是常数
+				if (!type1&&!type2)second="0";
+				else if (type1&&!type2) // x^a r.first是常数
 				{
 					cld cc = complex_calc(r.first);
 					if (l.first.size()!=1)
 					{
-						if (cc.imag()==0)second_ = r.first+"("+l.first+")";
-						else second_ = "("+to_string(cc.real())+"+"+to_string(cc.imag())+"i)("+l.first+")";
+						if (cc.imag()==0)second = r.first+"*("+l.first+")";
+						else second = "("+to_string(cc.real())+"+"+to_string(cc.imag())+"i)*("+l.first+")";
 					}
 					else
 					{
-						if (cc.imag()==0)second_ = r.first+l.first;
-						else second_ = "("+to_string(cc.real())+"+"+to_string(cc.imag())+"i)"+l.first;
+						if (cc.imag()==0)second = r.first+l.first;
+						else second = "("+to_string(cc.real())+"+"+to_string(cc.imag())+"i)*"+l.first;
 					}
 					if (cc!=cld(2))
 					{
-						if (cc.imag()==0) second_ += "^"+to_string(cc.real()-1); // 实数
-						else second_ += "^("+to_string(cc.real()-1)+"+"+to_string(cc.imag())+"i)";
+						if (cc.imag()==0) second += "^"+to_string(cc.real()-1); // 实数
+						else second += "^("+to_string(cc.real()-1)+"+"+to_string(cc.imag())+"i)";
 					}
 				}
 				else if (!type1&&type2) // a^x, l.first^r.first l.first^r.first*ln(l.first)*r.second
 				{
 					cld cc = complex_calc(l.first);
-					if (cc.imag()==0)
+					ld real = cc.real(), imag = cc.imag();
+					if (imag==0)
 					{
-						second_ = to_string(cc.real())+"^";
-						if (r.first.size()==1)second_+=r.first;
-						else second_+="("+r.first+")";
-						second_+="*ln("+to_string(cc.real())+")";
-						if (r.second!="1")second_+="("+r.second+")";
+						if (ll(real)==real)second = to_string(ll(real))+"^";
+						else second = to_string(cc.real())+"^";
+						if (r.first.size()==1)second+=r.first;
+						else second+="("+r.first+")";
+						if (ll(real)!=real)second+="*ln("+to_string(cc.real())+")";
+						else second += "*ln("+to_string((ll)cc.real())+")";
+						if (r.second!="1")second+="("+r.second+")";
 					}else
 					{
-						second_ = "("+to_string(cc.real())+"+"+to_string(cc.imag())+"i)^";
-						if (r.first.size()==1)second_+=r.first;
-						else second_+="("+r.first+")";
-						second_+="*ln("+to_string(cc.real())+"+"+to_string(cc.imag())+"i)";
-						if (r.second!="1")second_+="("+r.second+")";
+						second = "("+to_string(cc.real())+"+"+to_string(cc.imag())+"i)^";
+						if (r.first.size()==1)second+=r.first;
+						else second+="("+r.first+")";
+						second+="*ln("+to_string(cc.real())+"+"+to_string(cc.imag())+"i)";
+						if (r.second!="1")second+="("+r.second+")";
 					}
-					if (cc==cld(1)||cc==cld(0))second_="0";
+					if (cc==cld(1)||cc==cld(0))second="0";
 				}
 				else // f(x)^g(x)
 				{
-					if (l.first.size()==1)second_+=l.first+"^";
-					else second_+="("+l.first+")^";
-					if (r.first.size()==1)second_+=r.first+"*(";
-					else second_+="("+r.first+")*(";
+					if (l.first.size()==1)second+=l.first+"^";
+					else second+="("+l.first+")^";
+					if (r.first.size()==1)second+=r.first+"*(";
+					else second+="("+r.first+")*(";
 					if (l.first!="1"){
-						if (r.second.find_first_not_of("0123456789") != string::npos)second_+="("+r.second+")";
-						else if (r.second!="1")second_+=r.second;
-						if (r.second!="0")second_+="ln("+l.first+")+";
+						if (r.second.find_first_not_of("0123456789") != string::npos)second+="("+r.second+")";
+						else if (r.second!="1")second+=r.second;
+						if (r.second!="0")second+="ln("+l.first+")+";
 					}
-					if (r.first==l.first)second_+="1";
+					if (r.first==l.first)second+="1";
 					else
 					{
-						if (r.first.size()!=1)second_+="("+r.first+")";
-						else second_+=r.first;
-						if (l.second.size()!=1)second_+="("+l.second+")";
-						else if (l.second!="1")second_+="*"+l.second;
-						second_+="/";
-						if (l.first.size()!=1)second_+="("+l.first+")";
-						else second_+=l.first;
+						if (r.first.size()!=1)second+="("+r.first+")";
+						else second+=r.first;
+						if (l.second.size()!=1)second+="("+l.second+")";
+						else if (l.second!="1")second+="*"+l.second;
+						second+="/";
+						if (l.first.size()!=1)second+="("+l.first+")";
+						else second+=l.first;
 					}
 					
-					second_+=")";
+					second+=")";
 				}
-				result = {first_,second_};
+				result = {first,second};
 			}
 			stk.push(result);
 		}
@@ -974,17 +822,15 @@ inline string diff_calc_fu(string str,string var_name)
 			string z=x.first;
 			string dz = x.second;
 			if (token=="sin")
+			{
+				string hou;
+				if (dz!="0")
 				{
-					string hou;
-					if (dz!="0")
-					{
-						hou="cos("+z+")";
-						if (dz!="1")hou+="*("+dz+")";
-					}
-					else hou="0";
-					
-					res = {"sin("+z+")",hou};
-				}
+					hou="cos("+z+")";
+					if (dz!="1")hou+="*("+dz+")";
+				}else hou="0";
+				res = {"sin("+z+")",hou};
+			}
 			else if (token=="cos")
 				{
 					string hou;
